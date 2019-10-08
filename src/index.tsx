b281855b48882e55906a25b8b3623aba6a884780
Jonathan Rouleau <jonathan@rouleau.io>
2019-10-08 11:53:02 -0600

    Add primereact and styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
+import 'primereact/resources/themes/nova-light/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
 import './index.css'
 
 import React from 'react'
