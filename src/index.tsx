3a4e8418b721c14c5455a9731c08063a354b2dc5
Jonathan Rouleau <jonathan@rouleau.io>
2019-10-11 12:03:10 -0600

    Create splash screen

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,9 @@ import App from './app'
 
 /**********************************************************************/
 
-ReactDOM.render(<App />, document.getElementById('root'))
+setTimeout(() => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}, 1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
